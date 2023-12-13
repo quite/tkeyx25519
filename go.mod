9ea7768b630e0742edf57a81e0070a861027bad1
Daniel Lublin <daniel@lublin.se>
2023-12-13 23:45:08 +0100

    Rework ...
    - Try checking all-zero here; dont dump precious stuff!
    - Update for revised input data, protocol
    - Refactor
    - Bump dependencies

--- go.mod
+++ go.mod
@@ -2,13 +2,15 @@ module github.com/quite/tkeyx25519
 
 go 1.19
 
-require github.com/tillitis/tkeyclient v0.0.0-20230607181239-48de67d61ab9
+require (
+	github.com/tillitis/tkeyclient v0.0.8
+	golang.org/x/crypto v0.16.0
+)
 
 require (
 	github.com/creack/goselect v0.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	go.bug.st/serial v1.5.0 // indirect
-	golang.org/x/crypto v0.9.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
+	go.bug.st/serial v1.6.1 // indirect
+	golang.org/x/sys v0.15.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- tkeyx25519.go
+++ tkeyx25519.go
@@ -6,9 +6,31 @@ package tkeyx25519
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 
 	"github.com/tillitis/tkeyclient"
+	"golang.org/x/crypto/blake2s"
+)
+
+const UserSecretSize = 32
+
+type ResponseStatusNotOKError struct {
+	code byte
+}
+
+func (e *ResponseStatusNotOKError) Error() string {
+	return fmt.Sprintf("response status not OK, code: %d", e.code)
+}
+
+func (e *ResponseStatusNotOKError) Code() byte {
+	return e.code
+}
+
+const (
+	StatusOK           = byte(0)
+	StatusWrongCmdLen  = byte(1)
+	StatusTouchTimeout = byte(2)
 )
 
 var (
@@ -16,8 +38,8 @@ var (
 	rspGetNameVersion = appCmd{0x02, "rspGetNameVersion", tkeyclient.CmdLen32}
 	cmdGetPubKey      = appCmd{0x03, "cmdGetPubKey", tkeyclient.CmdLen128}
 	rspGetPubKey      = appCmd{0x04, "rspGetPubKey", tkeyclient.CmdLen128}
-	cmdComputeShared  = appCmd{0x05, "cmdComputeShared", tkeyclient.CmdLen128}
-	rspComputeShared  = appCmd{0x06, "rspComputeShared", tkeyclient.CmdLen128}
+	cmdDoECDH         = appCmd{0x05, "cmdDoECDH", tkeyclient.CmdLen128}
+	rspDoECDH         = appCmd{0x06, "rspDoECDH", tkeyclient.CmdLen128}
 )
 
 type appCmd struct {
@@ -62,120 +84,136 @@ func (x X25519) Close() error {
 	return nil
 }
 
-// GetAppNameVersion gets the name and version of the running app in
-// the same style as the stick itself.
+// GetAppNameVersion talks to the device app running on the TKey,
+// getting its name and version. A timeout is used to avoid hanging if
+// the device is running an app which does not handle the command, or
+// is in firmware mode.
 func (x X25519) GetAppNameVersion() (*tkeyclient.NameVersion, error) {
-	id := 2
-	tx, err := tkeyclient.NewFrameBuf(cmdGetNameVersion, id)
-	if err != nil {
-		return nil, fmt.Errorf("NewFrameBuf: %w", err)
-	}
-
-	tkeyclient.Dump("GetAppNameVersion tx", tx)
-	if err = x.tk.Write(tx); err != nil {
-		return nil, fmt.Errorf("Write: %w", err)
-	}
-
-	err = x.tk.SetReadTimeout(2)
-	if err != nil {
+	if err := x.tk.SetReadTimeout(2); err != nil {
 		return nil, fmt.Errorf("SetReadTimeout: %w", err)
 	}
 
-	rx, _, err := x.tk.ReadFrame(rspGetNameVersion, id)
+	rx, err := x.sendCommand(cmdGetNameVersion, bytes.Buffer{}, rspGetNameVersion)
 	if err != nil {
-		return nil, fmt.Errorf("ReadFrame: %w", err)
+		return nil, err
 	}
 
-	err = x.tk.SetReadTimeout(0)
-	if err != nil {
+	if err = x.tk.SetReadTimeout(0); err != nil {
 		return nil, fmt.Errorf("SetReadTimeout: %w", err)
 	}
 
 	nameVer := &tkeyclient.NameVersion{}
-	nameVer.Unpack(rx[2:])
+	nameVer.Unpack(rx[:12])
 
 	return nameVer, nil
 }
 
-// GetPubKey talks to the device app running on the TKey, getting a
-// X25519 public key. This public key is derived from a secret =
-// blake2s(domain, userSecret, require_touch, TKey CDI). requireTouch
-// is always part of the secret, but only the ComputeShared command
-// actually requests it.
-func (x X25519) GetPubKey(domain [78]byte, userSecret [16]byte, requireTouch bool) ([]byte, error) {
-	id := 2
-	tx, err := tkeyclient.NewFrameBuf(cmdGetPubKey, id)
+// GetPubKey talks to the X25519 device app running on the TKey to
+// retrieve a X25519 public key. The public key is derived by the
+// device app after hashing "private_key = blake2s(CDI, domain,
+// userSecret, requireTouch)". "CDI" is a base secret for use by the
+// app, see https://dev.tillitis.se/intro/. "domain" comes from
+// domainString, which is hashed using blake2s if the string was
+// longer than 32 bytes. "userSecret" is for identity/personalization
+// and must be high-entropy random. "requireTouch" indicates whether
+// the TKey should require physical touch when doing ECDH to create
+// the shared secret.
+func (x X25519) GetPubKey(domainString string, userSecret [UserSecretSize]byte, requireTouch bool) ([]byte, error) {
+	data := keyParameters(domainString, userSecret, requireTouch)
+
+	rx, err := x.sendCommand(cmdGetPubKey, data, rspGetPubKey)
 	if err != nil {
-		return nil, fmt.Errorf("NewFrameBuf: %w", err)
+		return nil, err
 	}
 
-	var buf bytes.Buffer
-	buf.Write(domain[:])
-	buf.Write(userSecret[:])
-	if requireTouch {
-		buf.WriteByte(1)
-	} else {
-		buf.WriteByte(0)
-	}
-	copy(tx[2:], buf.Bytes())
-
-	tkeyclient.Dump("GetPubKey tx", tx)
-	if err = x.tk.Write(tx); err != nil {
-		return nil, fmt.Errorf("Write: %w", err)
-	}
-
-	rx, _, err := x.tk.ReadFrame(rspGetPubKey, id)
-	tkeyclient.Dump("GetPubKey rx", rx)
-	if err != nil {
-		return nil, fmt.Errorf("ReadFrame: %w", err)
-	}
-
-	if rx[2] != tkeyclient.StatusOK {
-		return nil, fmt.Errorf("GetPubKey NOK")
-	}
-
-	// Skipping frame header, app header, and status
-	return rx[3 : 3+32], nil
+	return rx[:32], nil
 }
 
-// GetPubKey talks to the device app running on the TKey, establishing
-// a shared secret between theirPubKey and a TKey public key. This
-// public key is derived as for GetPubKey. requireTouch is always part
-// of the secret, and only this ComputeShared command actually
-// requests it.
-func (x X25519) ComputeShared(domain [78]byte, userSecret [16]byte, requireTouch bool, theirPubKey [32]byte) ([]byte, error) {
+// DoECDH talks to the X25519 device app running on the TKey to run
+// the ECDH (Elliptic-Curve Diffie-Hellman) function for establishing
+// a shared secret between theirPubKey and a private key. The private
+// key is hashed using the arguments in the same way as is done for
+// GetPubKey.
+func (x X25519) DoECDH(domainString string, userSecret [UserSecretSize]byte, requireTouch bool, theirPubKey [32]byte) ([]byte, error) {
+	data := keyParameters(domainString, userSecret, requireTouch)
+	data.Write(theirPubKey[:])
+
+	rx, err := x.sendCommand(cmdDoECDH, data, rspDoECDH)
+	if err != nil {
+		return nil, err
+	}
+
+	sharedSecret := rx[:32]
+
+	if isAllZero(sharedSecret) {
+		return nil, errors.New("result is all-zero due to small order point in input")
+	}
+
+	return sharedSecret, nil
+}
+
+func (x X25519) sendCommand(cmd appCmd, data bytes.Buffer, rsp appCmd) ([]byte, error) {
 	id := 2
-	tx, err := tkeyclient.NewFrameBuf(cmdComputeShared, id)
+	tx, err := tkeyclient.NewFrameBuf(cmd, id)
 	if err != nil {
 		return nil, fmt.Errorf("NewFrameBuf: %w", err)
 	}
 
+	// Place data after frame header byte and cmd code byte
+	if data.Len() > (len(tx) - 2) {
+		return nil, fmt.Errorf("data too large (%d > %d-2)", data.Len(), len(tx))
+	}
+	copy(tx[2:], data.Bytes())
+
+	if err = x.tk.Write(tx); err != nil {
+		return nil, fmt.Errorf("Write: %w", err)
+	}
+
+	rx, _, err := x.tk.ReadFrame(rsp, id)
+	if err != nil {
+		return nil, fmt.Errorf("ReadFrame: %w", err)
+	}
+
+	// This response contains no status code
+	if rsp.code == rspGetNameVersion.code {
+		// Skipping over frame header byte, and rsp code byte
+		return rx[2:], nil
+	}
+
+	if rx[2] != StatusOK {
+		return nil, &ResponseStatusNotOKError{code: rx[2]}
+	}
+
+	// Skipping over frame header byte, rsp code byte, and status byte
+	return rx[3:], nil
+}
+
+func keyParameters(domainString string, userSecret [UserSecretSize]byte, requireTouch bool) bytes.Buffer {
 	var buf bytes.Buffer
+
+	var domain [32]byte
+	if len(domainString) > 32 {
+		domain = blake2s.Sum256([]byte(domainString))
+	} else {
+		copy(domain[:], []byte(domainString))
+	}
 	buf.Write(domain[:])
+
 	buf.Write(userSecret[:])
+
 	if requireTouch {
 		buf.WriteByte(1)
 	} else {
 		buf.WriteByte(0)
 	}
-	buf.Write(theirPubKey[:])
-	copy(tx[2:], buf.Bytes())
 
-	tkeyclient.Dump("ComputeShared tx", tx)
-	if err = x.tk.Write(tx); err != nil {
-		return nil, fmt.Errorf("Write: %w", err)
+	return buf
+}
+
+func isAllZero(bytes []byte) bool {
+	var accu byte
+	for _, b := range bytes {
+		accu |= b
 	}
-
-	rx, _, err := x.tk.ReadFrame(rspComputeShared, id)
-	tkeyclient.Dump("ComputeShared rx", rx)
-	if err != nil {
-		return nil, fmt.Errorf("ReadFrame: %w", err)
-	}
-
-	if rx[2] != tkeyclient.StatusOK {
-		return nil, fmt.Errorf("ComputeShared NOK")
-	}
-
-	// Skipping frame header, app header, and status
-	return rx[3 : 3+32], nil
+	return accu == 0
 }
